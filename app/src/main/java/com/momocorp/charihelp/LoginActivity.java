af19914c8c3ae8fb3dce9131e9d4e2f59c0654df
root <root@DESKTOP-8QK1TEU.localdomain>
2017-11-28 04:53:26 -0500

    Committing additional files. Don't remember changes

--- app/src/main/java/com/momocorp/charihelp/LoginActivity.java
+++ app/src/main/java/com/momocorp/charihelp/LoginActivity.java
@@ -1,4 +1,5 @@
 package com.momocorp.charihelp;
+
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 public class LoginActivity extends AppCompatActivity {
     Button loginButton;
     TextView signUp;
@@ -35,13 +37,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginButton =(Button) findViewById(R.id.login_button);
+        loginButton = (Button) findViewById(R.id.login_button);
         email = (EditText) findViewById(R.id.email_address_edit);
         password = (EditText) findViewById(R.id.password_edit);
         email.setText("test_3@email.com");
         password.setText("Dragon");
         tutorSignIn = findViewById(R.id.sign_in_as_tutor);
-        signUp =  findViewById(R.id.sign_up);
+        signUp = findViewById(R.id.sign_up);
         auth = FirebaseAuth.getInstance();
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,43 +61,48 @@ public class LoginActivity extends AppCompatActivity {
                                                  @Override
                                                  public void onSuccess(AuthResult authResult) {
                                                      //get the user associated with the uid and run a query
-                                                     String uID = auth.getCurrentUser().getUid();
-                                                     DatabaseReference reference = FirebaseDatabase.
-                                                             getInstance().getReference();
-                                                     reference.child("users").orderByChild("uid").equalTo(uID).
-                                                             addChildEventListener(new ChildEventListener() {
-                                                                 @Override
-                                                                 public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                                                                     Log.i("Firebase Test", dataSnapshot.getValue().toString());
-                                                                     LoginActivity.user =
-                                                                             dataSnapshot.getValue(User.class);
-                                                                     startActivity(new Intent(LoginActivity.this, UserActivity.class));
-                                                                     finish();
+                                                     FirebaseUser user = auth.getCurrentUser();
 
-                                                                 }
+                                                     if (user != null) {
+                                                         String uID = user.getUid();
+                                                         DatabaseReference reference = FirebaseDatabase.
+                                                                 getInstance().getReference();
+                                                         reference.child("users").orderByChild("uid").equalTo(uID).
+                                                                 addChildEventListener(new ChildEventListener() {
+                                                                     @Override
+                                                                     public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                                                                         Log.i("Firebase Test", dataSnapshot.getValue().toString());
+                                                                         LoginActivity.user =
+                                                                                 dataSnapshot.getValue(User.class);
+                                                                         startActivity(new Intent(LoginActivity.this, UserActivity.class));
+                                                                         finish();
 
-                                                                 @Override
-                                                                 public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                                                                     Log.i("Firebase Test", dataSnapshot.getValue().toString());
-                                                                     LoginActivity.user = dataSnapshot.getValue(User.class);
-                                                                 }
+                                                                     }
 
-                                                                 @Override
-                                                                 public void onChildRemoved(DataSnapshot dataSnapshot) {
+                                                                     @Override
+                                                                     public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                                                                         Log.i("Firebase Test", dataSnapshot.getValue().toString());
+                                                                         LoginActivity.user = dataSnapshot.getValue(User.class);
+                                                                     }
 
-                                                                 }
+                                                                     @Override
+                                                                     public void onChildRemoved(DataSnapshot dataSnapshot) {
 
-                                                                 @Override
-                                                                 public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-                                                                     Log.i("Firebase Test", dataSnapshot.getValue().toString());
-                                                                     LoginActivity.user = dataSnapshot.getValue(User.class);
-                                                                 }
+                                                                     }
 
-                                                                 @Override
-                                                                 public void onCancelled(DatabaseError databaseError) {
+                                                                     @Override
+                                                                     public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                                                                         Log.i("Firebase Test", dataSnapshot.getValue().toString());
+                                                                         LoginActivity.user = dataSnapshot.getValue(User.class);
+                                                                     }
 
-                                                                 }
-                                                             });
+                                                                     @Override
+                                                                     public void onCancelled(DatabaseError databaseError) {
+
+                                                                     }
+                                                                 });
+
+                                                     }
 
                                                  }
                                              }
